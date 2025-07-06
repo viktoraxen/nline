347643c149f2bbd5e54bfc635258fd8987705a17
Viktor Axén <vikax460@student.liu.se>
2025-07-06 23:40:19 +0200

    highlight: fix crash when group was nil

--- lua/nline/components/mode.lua
+++ lua/nline/components/mode.lua
@@ -59,22 +59,17 @@ M.state = {
 
 M.text = function()
     local align = require("nline.utils").align
+    local highlight = require("nline.utils").highlight
 
     local padding = string.rep(" ", M.config.padding)
     local text_width = M.config.width - 2 * M.config.padding
 
-    local highlight = M.highlight_map[M.state.mode]
-
-    if not highlight then
-        vim.notify(M.state.mode)
-    end
+    local group = M.highlight_map[M.state.mode]
 
     return table.concat({
-        "%#" .. highlight .. "#",
         padding,
-        align(texts[M.state.mode], text_width, "left"),
+        highlight(texts[M.state.mode], group),
         padding,
-        "%##"
     })
 end
 
--- lua/nline/utils.lua
+++ lua/nline/utils.lua
@@ -1,40 +1,16 @@
 local M = {}
 
 M.highlight = function(text, group)
+    if not group then
+        if text then return text end
+        return ""
+    end
+
     return table.concat({
         "%#" .. group .. "#",
         text,
         "%##",
     })
-end
-
-M.pad = function(text, min_width, left_pad, right_pad)
-    local left_padding = string.rep(" ", left_pad)
-    local right_padding = string.rep(" ", right_pad)
-    local text_width = min_width - (left_pad + right_pad)
-
-    return table.concat({
-        left_padding,
-        text,
-        right_padding
-    })
-end
-
-M.align = function(text, width, dir)
-    if not text then return string.rep(" ", width) end
-
-    local padding = string.rep(" ", width - text:len())
-
-    if dir == "left" then
-        return table.concat({ text, padding })
-    elseif dir == "right" then
-        return table.concat({ padding, text })
-    end
-
-    local left = string.rep(" ", (width - text:len()) / 2)
-    local right = string.rep(" ", 1 + (width - text:len()) / 2)
-
-    return table.concat({ left, text, right })
 end
 
 M.concat = function(table1, table2)
